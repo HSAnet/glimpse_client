a2813e5af94cab372fe015e9f7643981e3e5f081
Christoph Keller <gri@not-censored.com>
2013-04-29 13:37:49 +0200

    Quit the application when qml engine wants to.

--- src/mobile/main.cpp
+++ src/mobile/main.cpp
@@ -4,6 +4,7 @@
 #include <QGuiApplication>
 #include <QQuickView>
 #include <QQmlContext>
+#include <QQmlEngine>
 #include <QtQml>
 #include <QUrl>
 
@@ -16,6 +17,9 @@ int main(int argc, char* argv[])
     qmlRegisterUncreatableType<TestScheduler>("mplane", 1, 0, "TestScheduler", "uncreatable type");
 
     QQuickView view;
+
+    QQmlEngine* engine = view.engine();
+    QObject::connect(engine, SIGNAL(quit()), &app, SLOT(quit()));
 
     QQmlContext* rootContext = view.rootContext();
     rootContext->setContextProperty("client", Client::instance());
