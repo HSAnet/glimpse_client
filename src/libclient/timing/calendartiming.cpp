7d51ce28dcc4bc0ecf7f1246ec218ac0a58efa18
Christoph Keller <gri@not-censored.com>
2013-10-05 13:34:14 +0200

    Provisional implementation for TimingRandomness

--- src/libclient/timing/calendartiming.cpp
+++ src/libclient/timing/calendartiming.cpp
@@ -1,8 +1,65 @@
 #include "calendartiming.h"
 
+class TimingRandomness::Private
+{
+public:
+    QString otherDistribution; // Filled with other values
+    TimingRandomness::DistributionType distribution;
+    int upperCut;
+    int lowerCut;
+    int spread;
+};
+
+TimingRandomness::TimingRandomness()
+: d(new Private)
+{
+}
+
+TimingRandomness::~TimingRandomness()
+{
+    delete d;
+}
+
+QString TimingRandomness::otherDistribution() const
+{
+    return d->otherDistribution;
+}
+
+TimingRandomness::DistributionType TimingRandomness::distribution() const
+{
+    return d->distribution;
+}
+
+int TimingRandomness::upperCut() const
+{
+    return d->upperCut;
+}
+
+int TimingRandomness::lowerCut() const
+{
+    return d->lowerCut;
+}
+
+int TimingRandomness::spread() const
+{
+    return d->spread;
+}
+
+QVariant TimingRandomness::toVariant() const
+{
+    QVariantMap map;
+    map.insert("distribution", ""); // TODO: Fill
+    map.insert("upperCut", d->upperCut);
+    map.insert("lowerCut", d->lowerCut);
+    map.insert("spread", d->spread);
+    return map;
+}
+
+
 class CalendarTiming::Private
 {
 public:
+    TimingRandomness randomness;
 };
 
 CalendarTiming::CalendarTiming()
@@ -25,13 +82,19 @@ QDateTime CalendarTiming::nextRun() const {
 
 QVariant CalendarTiming::toVariant() const {
     QVariantMap hash;
-    // TODO: Implementation
+    hash.insert("type", type());
+    hash.insert("randomness", d->randomness.toVariant());
     return hash;
 }
 
 TimingPtr CalendarTiming::fromVariant(const QVariant &variant) {
     Q_UNUSED(variant);
     return TimingPtr(new CalendarTiming);
+}
+
+TimingRandomness CalendarTiming::randomness() const
+{
+    return d->randomness;
 }
 
 QString CalendarTiming::type() const
