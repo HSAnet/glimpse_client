803ddc8b3ac741d49a347c35a05f0654b0443cb3
Christoph Keller <gri@not-censored.com>
2013-10-21 18:00:43 +0200

    Extend report files on disk when new results are available

--- src/libclient/client.cpp
+++ src/libclient/client.cpp
@@ -59,7 +59,9 @@ public slots:
 
 void Client::Private::taskFinished(const TestDefinitionPtr &test, const ResultPtr &result)
 {
-    ResultList results;
+    ReportPtr oldReport = reportScheduler.reportByTaskId(test->id());
+
+    ResultList results = oldReport.isNull() ? ResultList() : oldReport->results();
     results.append(result);
 
     ReportPtr report(new Report(test->id(), QDateTime::currentDateTime(), results));
@@ -119,7 +121,7 @@ void Client::btc()
 void Client::upnp()
 {
     TimingPtr timing(new OnOffTiming(QDateTime::currentDateTime().addSecs(5)));
-    TestDefinitionPtr testDefinition(new TestDefinition(QUuid::createUuid(), "upnp", timing, QVariant()));
+    TestDefinitionPtr testDefinition(new TestDefinition("{3702e527-f84f-4542-8df6-4e3d2a0ec977}", "upnp", timing, QVariant()));
     d->scheduler.enqueue(testDefinition);
 }
 
--- src/libclient/report/reportscheduler.cpp
+++ src/libclient/report/reportscheduler.cpp
@@ -1,5 +1,7 @@
 #include "reportscheduler.h"
 
+#include <QUuid>
+
 class ReportScheduler::Private
 {
 public:
@@ -16,6 +18,16 @@ ReportScheduler::~ReportScheduler()
     delete d;
 }
 
+ReportPtr ReportScheduler::reportByTaskId(const QUuid &taskId) const
+{
+    foreach(const ReportPtr& r, d->reports) {
+        if (r->taskId() == taskId)
+            return r;
+    }
+
+    return ReportPtr();
+}
+
 ReportList ReportScheduler::reports() const
 {
     return d->reports;
@@ -23,6 +35,16 @@ ReportList ReportScheduler::reports() const
 
 void ReportScheduler::addReport(const ReportPtr &report)
 {
+    for(int i=0; i < d->reports.size(); ++i) {
+        const ReportPtr& r = d->reports.at(i);
+
+        if (r->taskId() == report->taskId()) {
+            d->reports.replace(i, report);
+            emit reportAdded(report);
+            return;
+        }
+    }
+
     d->reports.append(report);
     emit reportAdded(report);
 }
