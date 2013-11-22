e418aec23e3c05e64662dc337d6f1d975b38b804
Christoph Keller <gri@not-censored.com>
2013-11-22 13:36:46 +0100

    Use the cool translucent theme on android.

--- src/mobile/android/src/MobileActivity.java
+++ src/mobile/android/src/MobileActivity.java
@@ -1,11 +1,38 @@
 package de.hsaugsburg.informatik.mplane;
 
 import org.qtproject.qt5.android.bindings.QtActivity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MobileActivity extends QtActivity {
     public static MobileActivity instance = null;
 
     public MobileActivity() {
         instance = this;
+    }
+
+    public int getStatusBarHeight() {
+          int result = 0;
+          int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+          if (resourceId > 0) {
+              result = getResources().getDimensionPixelSize(resourceId);
+          }
+          return result;
+    }
+
+    protected void makeThemeTranslucent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        makeThemeTranslucent();
+
+        super.onCreate(savedInstanceState);
     }
 }
