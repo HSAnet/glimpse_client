79c2a8be039541e28c4cecfcceddfd03a906766c
Christoph Keller <gri@not-censored.com>
2014-01-20 15:10:57 +0100

    Added ruleExclusions for code style.

--- buildscripts/vera++.sh
+++ buildscripts/vera++.sh
@@ -4,6 +4,6 @@ cd `dirname $0`
 cd ..
 
 PROFILE_NAME="client"
-PARAMS="-P max-line-length=200"
+PARAMS="-P max-line-length=200 --exclusions buildscripts/vera++/ruleExclusions"
 
 find src/ -name "*.h" -or -name "*.c" -or -name "*.cpp" | vera++ --root buildscripts/vera++ --profile $PROFILE_NAME $PARAMS $@
