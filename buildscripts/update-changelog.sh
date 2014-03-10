ac74747ff181e8cae2167b616c1259ad6b9f2a49
Christoph Keller <gri@not-censored.com>
2014-03-10 16:38:49 +0100

    Modified update-changelog script to produce new packages for each build.

--- buildscripts/update-changelog.sh
+++ buildscripts/update-changelog.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
+MAINTAINER="Christoph Keller <gri@not-censored.com>"
+VERSION="0.1alpha1"
+TIMESTAMP=$(date -R)
+
+BUILDNUMBER=$1
+
 cd `dirname $0`
 cd ..
+cd debian
 
-git-dch --git-author -S -a
+if [ "$BUILDNUMBER" = "" ]; then
+	echo "You have no BUILDNUMBER variable set, exiting"
+	exit 1
+fi
+
+echo "glimpse ($VERSION-$BUILDNUMBER) unstable; urgency=low" > changelog
+echo "" >> changelog
+echo "  * Snapshot build #$BUILDNUMBER" >> changelog
+echo "" >> changelog
+echo " -- $MAINTAINER  $TIMESTAMP" >> changelog
+
+echo "Changelog for build #$BUILDNUMBER written."
 
