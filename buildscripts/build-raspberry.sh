b4034ce8f4789061061c19530b57637848655c5c
Christoph Keller <gri@not-censored.com>
2014-03-26 16:39:56 +0100

    Buildscript for raspberry pi chroot

---
+++ buildscripts/build-raspberry.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SSH="ssh root@localhost -p 20 --"
+
+case "$1" in
+	remote)
+	export LC_ALL=C
+	export PATH=/opt/qt52/bin:$PATH
+	cd /root/work/glimpse_client
+	dpkg-buildpackage -b
+	;;
+
+	*)
+	# Delete files on build host
+	$SSH rm -fr /root/work/*
+
+	# Copy files via scp
+	scp -q -P 20 -r . root@localhost:/root/work/
+
+	$SSH /root/work/glimpse_client/buildscripts/build-raspberry.sh remote
+
+	scp -q -P 20 -r root@localhost:/root/work/*.deb .
+;;
+esac
+
+
