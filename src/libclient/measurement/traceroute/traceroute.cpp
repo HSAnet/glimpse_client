5a2b14b86fe85d935162a9741462d9f8ce9c8d59
Thomas Hipp <thomashipp@gmail.com>
2014-04-22 15:58:42 +0200

    traceroute: add basic functionality

--- src/libclient/measurement/traceroute/traceroute.cpp
+++ src/libclient/measurement/traceroute/traceroute.cpp
@@ -1,12 +1,19 @@
 #include "../../log/logger.h"
 #include "traceroute.h"
 
+#if defined(Q_OS_LINUX) || defined(Q_OS_MAC)
+#include <arpa/inet.h>
+#elif defined(Q_OS_WIN)
+#include <winsock2.h>
+#endif
+
 LOGGER("Traceroute");
 
 Traceroute::Traceroute(QObject *parent) :
     Measurement(parent),
     currentStatus(Unknown),
-    udpPing()
+    udpPing(),
+    mutex()
 {
 }
 
@@ -34,18 +41,28 @@ bool Traceroute::prepare(NetworkManager* networkManager,
     Q_UNUSED(networkManager);
     definition = measurementDefinition.dynamicCast<TracerouteDefinition>();
 
-    connect(&udpPing, &UdpPing::destinationUnreachable,
-            this, &Traceroute::destinationUnreachable);
+    connect(&udpPing, SIGNAL(destinationUnreachable(PingProbe&)),
+            this, SLOT(destinationUnreachable(PingProbe&)));
 
-    connect(&udpPing, &UdpPing::ttlExceeded,
-            this, &Traceroute::ttlExceeded);
+    connect(&udpPing, SIGNAL(ttlExceeded(PingProbe&)),
+            this, SLOT(ttlExceeded(PingProbe&)));
+
+    connect(&udpPing, SIGNAL(timeout(PingProbe&)),
+            this, SLOT(timeout(PingProbe&)));
+
+    if (!mutex.tryLock(0))
+    {
+        mutex.unlock();
+        mutex.lock();
+    }
 
     return true;
 }
 
 bool Traceroute::start()
 {
-    return false;
+    traceroute();
+    return true;
 }
 
 bool Traceroute::stop()
@@ -56,16 +73,81 @@ bool Traceroute::stop()
 ResultPtr Traceroute::result() const
 {
     QVariantList res;
+    QVariantList pings;
+    QVariantMap hop;
+    QVariantMap probe;
+
+    for (int i = 0; i < hops.size(); i += definition->count)
+    {
+        pings.clear();
+
+        for (quint32 k = 0; k < definition->count; k++)
+        {
+            probe.insert("reponse", hops[i + k].response);
+            probe.insert("rtt", (int) (hops[i + k].probe.recvTime -
+                                       hops[i + k].probe.sendTime));
+            pings.append(probe);
+        }
+
+        hop.insert("hop", QString(inet_ntoa(hops[i].probe.source.sin.sin_addr)));
+        hop.insert("pings", pings);
+        hop.insert("ttl", i / 3 + 1);
+
+        res << hop;
+    }
 
     return ResultPtr(new Result(QDateTime::currentDateTime(), res, QVariant()));
 }
 
+void Traceroute::traceroute()
+{
+    // TODO: remove hard-coded maximum hops
+    for (quint32 i = 1; i < 30; i++)
+    {
+        ping(i);
+        mutex.lock();
+        if (hops.last().response == DESTINATION_UNREACHABLE)
+        {
+            break;
+        }
+    }
+
+    emit finished();
+}
+
+void Traceroute::ping(quint32 ttl)
+{
+    UdpPingDefinition udpPingDef(definition->url,
+                                 definition->count,
+                                 definition->interval,
+                                 definition->receiveTimeout,
+                                 ttl,
+                                 definition->destinationPort,
+                                 definition->sourcePort,
+                                 definition->payload);
+    udpPing.prepare(NULL, UdpPingPlugin().createMeasurementDefinition(
+                "udpping",
+                udpPingDef.toVariant()));
+    udpPing.start();
+}
+
 void Traceroute::destinationUnreachable(PingProbe &probe)
 {
-    LOG_DEBUG("destination unreachable");
+    Hop hop = {probe, DESTINATION_UNREACHABLE};
+    hops << hop;
+    mutex.unlock();
 }
 
 void Traceroute::ttlExceeded(PingProbe &probe)
 {
-    LOG_DEBUG("TTL exceeded");
+    Hop hop = {probe, TTL_EXCEEDED};
+    hops << hop;
+    mutex.unlock();
+}
+
+void Traceroute::timeout(PingProbe &probe)
+{
+    Hop hop = {probe, TIMEOUT};
+    hops << hop;
+    mutex.unlock();
 }
--- src/libclient/measurement/traceroute/traceroute.h
+++ src/libclient/measurement/traceroute/traceroute.h
@@ -1,9 +1,29 @@
 #ifndef TRACEROUTE_H
 #define TRACEROUTE_H
 
+#include <QtGlobal>
+#include <QMutex>
+#include <QVector>
+
 #include "../measurement.h"
+#include "../../task/task.h"
 #include "../udpping/udpping.h"
+#include "../udpping/udpping_plugin.h"
+#include "../udpping/udpping_definition.h"
 #include "traceroute_definition.h"
+
+enum Response
+{
+    TTL_EXCEEDED,
+    DESTINATION_UNREACHABLE,
+    TIMEOUT
+};
+
+struct Hop
+{
+    PingProbe probe;
+    Response response;
+};
 
 class Traceroute : public Measurement
 {
@@ -22,17 +42,23 @@ public:
 
 private:
     void setStatus(Status status);
+    void traceroute();
+    void ping(quint32 ttl);
 
     TracerouteDefinitionPtr definition;
     Status currentStatus;
     UdpPing udpPing;
+    QVector<Hop> hops;
+    QMutex mutex;
 
 signals:
     void statusChanged(Status status);
+    void handledResponse();
 
 public slots:
     void destinationUnreachable(PingProbe &probe);
     void ttlExceeded(PingProbe &probe);
+    void timeout(PingProbe &probe);
 };
 
 #endif // TRACEROUTE_H
