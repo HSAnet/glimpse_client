1900131975386c80725d838acb32b0d318453451
Thomas Hipp <thomashipp@gmail.com>
2014-04-23 17:20:45 +0200

    remove evil tabs :smiling_imp:

--- src/libclient/measurement/traceroute/traceroute_definition.cpp
+++ src/libclient/measurement/traceroute/traceroute_definition.cpp
@@ -1,12 +1,12 @@
 #include "traceroute_definition.h"
 
 TracerouteDefinition::TracerouteDefinition(const QString &url,
-					   const quint32 &count,
-					   const quint32 &interval,
-					   const quint32 &receiveTimeout,
-					   const quint16 &destinationPort,
-					   const quint16 &sourcePort,
-					   const quint32 &payload)
+                                           const quint32 &count,
+                                           const quint32 &interval,
+                                           const quint32 &receiveTimeout,
+                                           const quint16 &destinationPort,
+                                           const quint16 &sourcePort,
+                                           const quint32 &payload)
 : url(url),
 count(count),
 interval(interval),
@@ -25,13 +25,13 @@ TracerouteDefinitionPtr TracerouteDefinition::fromVariant(const QVariant &varian
 {
     QVariantMap map = variant.toMap();
     return TracerouteDefinitionPtr(new TracerouteDefinition(
-			    map.value("url").toString(),
-			    map.value("count").toUInt(),
-			    map.value("interval").toUInt(),
-			    map.value("receiveTimeout").toUInt(),
-			    map.value("destinationPort").toUInt(),
-			    map.value("sourcePort").toUInt(),
-			    map.value("payload").toUInt()));
+                            map.value("url").toString(),
+                            map.value("count").toUInt(),
+                            map.value("interval").toUInt(),
+                            map.value("receiveTimeout").toUInt(),
+                            map.value("destinationPort").toUInt(),
+                            map.value("sourcePort").toUInt(),
+                            map.value("payload").toUInt()));
 }
 
 QVariant TracerouteDefinition::toVariant() const
