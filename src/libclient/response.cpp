8c0090d813a561d0cfb10a91f2efed23d4eed69b
Michael Faath <michael.faath@hs-augsburg.de>
2014-06-28 15:26:43 +0200

    Response function for schedules

--- src/libclient/response.cpp
+++ src/libclient/response.cpp
@@ -186,24 +186,30 @@ bool GetInstructionResponse::fillFromVariant(const QVariantMap &variant)
 
 bool GetScheduleResponse::fillFromVariant(const QVariantMap &variant)
 {
-    QVariantMap variantMap = variant.value("object").toMap();
 
-    // validate task
-    TestDefinition test = TestDefinition::fromVariant(variantMap.value("task"));
-
-    // get timing and merge into task FIXME this is an evil hack
-    TimingPtr timing = TimingFactory::timingFromVariant(variantMap.value("timing"));
-    test.setTiming(timing);
-
-    if (m_validator.validate(test) == TaskValidator::Valid)
+    foreach (const QVariant &entry, variant.value("objects").toList())
     {
-        m_tasks.append(test);
+        QVariantMap variantMap = entry.toMap();
+
+        // generate task
+        TestDefinition test = TestDefinition::fromVariant(variantMap.value("task"));
+
+        // get timing and merge into task FIXME this is an evil hack
+        TimingPtr timing = TimingFactory::timingFromVariant(variantMap.value("timing"));
+        test.setTiming(timing);
+
+        if (m_validator.validate(test) == TaskValidator::Valid)
+        {
+            m_tasks.append(test);
+        }
+        else
+        {
+            // TODO: Get more information
+            LOG_DEBUG(QString("Received invalid task, ignoring."));
+        }
     }
-    else
-    {
-        // TODO: Get more information
-        LOG_DEBUG(QString("Received invalid task, ignoring."));
-    }
+
+
 
     return true;
 }
