7ced4404bbd8b766601d3ee9203f4c001d3f1ec9
Christoph Keller <gri@not-censored.com>
2014-08-20 15:15:53 +0200

    New upload-package script.

--- buildscripts/upload-package.sh
+++ buildscripts/upload-package.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PACKAGESERVER="repository@debmirror.informatik.hs-augsburg.de"
-REPOSITORYPATH="repository"
+PACKAGESERVER="http://distributor.measure-it.net/api/upload/"
 
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 <branch>"
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <branch> <distribution>"
 	exit 1
 fi
 
 BRANCH=$1
+DISTRO=$2
 
 if [ "$BRANCH" != "develop" ]; then
 	echo "Nothing to upload for branch $BRANCH"
@@ -19,9 +19,10 @@ cd `dirname $0`
 cd ..
 cd ..
 
-# Copy files via scp
-scp *_amd64.deb $PACKAGESERVER:/home/repository/$REPOSITORYPATH/binary || true
-scp *_armhf.deb $PACKAGESERVER:/home/repository/$REPOSITORYPATH/raspberrypi || true
+# Upload each file to the distributor server
+for file in *.deb *.rpm *.tar.xz *.apk *.exe *.dmg; do
+	if [ -e $file ]; then
+		curl --form package=@$file --form channel=$DISTRO $PACKAGESERVER
+	fi
+done
 
-# Update the Packages.gz
-ssh $PACKAGESERVER "$REPOSITORYPATH/update_packages.sh"
