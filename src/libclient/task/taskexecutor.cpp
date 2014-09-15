971719895152463c47143c25ab8af8a13e645087
Thomas Hipp <thomas.hipp@hs-augsburg.de>
2014-09-15 12:51:18 +0200

    move interface check to internal taskexecutor

--- src/libclient/task/taskexecutor.cpp
+++ src/libclient/task/taskexecutor.cpp
@@ -33,11 +33,19 @@ public slots:
         if (!measurement.isNull())
         {
             connect(measurement.data(), SIGNAL(finished()), this, SLOT(measurementFinished()));
+            connect(measurement.data(), SIGNAL(error(const QString &)), this, SLOT(measurementError(const QString &)));
 
             if (observer)
             {
                 observer->created(measurement);
                 delete observer;
+            }
+
+            if (networkManager->allInterfacesDown())
+            {
+                LOG_ERROR(QString("Unable to execute measurement, no interface is up: %1").arg(test.name()));
+                measurementError("No interface is up");
+                return;
             }
 
             MeasurementDefinitionPtr definition = factory.createMeasurementDefinition(test.name(), test.measurementDefinition());
@@ -78,7 +86,7 @@ public slots:
         // TODO1: get local information (interface with ip, connection type, SNR, cpu usage, ...) and save in result or measurement
 
         measurement->disconnect(this, SLOT(measurementFinished()));
-        measurement->disconnect(this, SLOT(measurementError(QString)));
+        measurement->disconnect(this, SLOT(measurementError(const QString &)));
 
         LOG_INFO(QString("Finished execution of %1 (success)").arg(currentTest.name()));
 
@@ -87,10 +95,10 @@ public slots:
         measurement.clear();
     }
 
-    void measurementError(QString errorMsg)
+    void measurementError(const QString &errorMsg)
     {
         measurement->disconnect(this, SLOT(measurementFinished()));
-        measurement->disconnect(this, SLOT(measurementError()));
+        measurement->disconnect(this, SLOT(measurementError(const QString &)));
 
         LOG_ERROR(QString("Finished execution of %1 (failed): %2").arg(currentTest.name()).arg(errorMsg));
 
@@ -217,7 +225,7 @@ void TaskExecutor::execute(const TestDefinition &test, MeasurementObserver *obse
     }
 
     // Abort if we are on a mobile connection
-    if (d->executor.networkManager->onMobileConnection() || d->executor.networkManager->allInterfacesDown())
+    if (d->executor.networkManager->onMobileConnection())
     {
         LOG_ERROR(QString("Unable to execute measurement, we are on a mobile connection or no interface is up: %1").arg(test.name()));
         return;
