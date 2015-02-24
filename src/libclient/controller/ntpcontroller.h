9f9ab22e53b2c53f4fddfce70c647c8ae5940b7e
Thomas Hipp <thomas.hipp@hs-augsburg.de>
2015-02-25 10:45:20 +1300

    ntp: move structs to cpp file

--- src/libclient/controller/ntpcontroller.h
+++ src/libclient/controller/ntpcontroller.h
@@ -8,57 +8,6 @@
 #include <QObject>
 #include <QUdpSocket>
 
-struct NtpTimestamp
-{
-    quint32 seconds;
-    quint32 fraction;
-
-    static QDateTime toDateTime(const NtpTimestamp &timestamp)
-    {
-        quint32 seconds = qFromBigEndian(timestamp.seconds);
-        quint32 fraction = qFromBigEndian(timestamp.fraction);
-        qint64 ms = seconds * 1000ll + fraction * 1000ll / 0x100000000ll;
-
-        return QDateTime::fromMSecsSinceEpoch(ms - 2208988800000ll);
-    }
-
-    static NtpTimestamp fromDateTime(const QDateTime &timestamp)
-    {
-        NtpTimestamp result;
-        qint64 ms = timestamp.toMSecsSinceEpoch() + 2208988800000ll;
-        quint32 seconds = ms / 1000;
-        quint32 fraction = 0x100000000ll * (ms % 1000) / 1000;
-
-        result.seconds = qToBigEndian(seconds);
-        result.fraction = qToBigEndian(fraction);
-
-        return result;
-    }
-};
-
-struct NtpPacket
-{
-    struct NtpFlags
-    {
-        quint8 mode: 3;
-        quint8 versionNumber: 3;
-        quint8 leapIndicator: 2;
-    } flags;
-
-    quint8 peerClockStratum;
-    quint8 peerPollingInterval;
-    quint8 peerClockPrecision;
-
-    quint32 rootDelay;
-    quint32 rootDispersion;
-    quint32 referenceId;
-
-    NtpTimestamp referenceTimestamp;
-    NtpTimestamp originTimestamp;
-    NtpTimestamp receiveTimestamp;
-    NtpTimestamp transmitTimestamp;
-};
-
 class CLIENT_API NtpController : public Controller
 {
     Q_OBJECT
