5ab752588b1c57f31d85dfa209683b919b154a24
Thomas Hipp <thomas.hipp@hs-augsburg.de>
2015-09-27 09:34:29 +0200

    bump version

--- src/libclient/client.h
+++ src/libclient/client.h
@@ -95,7 +95,7 @@ public:
      */
     static const quint32 versionMajor = 2;
     static const quint32 versionMinor = 7;
-    static const quint32 versionPatch = 0;
+    static const quint32 versionPatch = 1;
 
 public slots:
     bool init();
